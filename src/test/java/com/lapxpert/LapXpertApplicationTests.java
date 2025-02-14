9d23179e9f4773f2999321109fad6a77530f2b5e
Khoa Đăng <khoalda.dev@gmail.com>
2025-02-14 20:22:50 +0700

    init

---
+++ src/test/java/com/lapxpert/LapXpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapxpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapXpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
