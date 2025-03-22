513cbe1550c841a2650c27db39e391f641906a88
Khoa Đăng <khoalda.dev@gmail.com>
2025-03-23 00:21:35 +0700

    Merge thongke

---
+++ src/main/java/com/lapxpert/thongke/domain/repository/HoaDonRepository.java
@@ -0,0 +1,7 @@
+package com.lapxpert.thongke.domain.repository;
+
+import com.lapxpert.thongke.domain.entity.HoaDon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoaDonRepository extends JpaRepository<HoaDon, Integer> {
+}
---
+++ src/main/java/com/lapxpert/thongke/domain/service/HoaDonService.java
@@ -0,0 +1,32 @@
+package com.lapxpert.thongke.domain.service;
+
+import com.lapxpert.thongke.domain.entity.HoaDon;
+import com.lapxpert.thongke.domain.repository.HoaDonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class HoaDonService {
+    @Autowired
+    HoaDonRepository hoaDonRepository;
+
+    public List<HoaDon> getAll() {
+        return hoaDonRepository.findAll();
+    }
+
+    public BigDecimal getTongTien() {
+        BigDecimal tongTien = BigDecimal.ZERO;
+        for (HoaDon hoaDon : getAll()) {
+            tongTien = tongTien.add(hoaDon.getTongTien()); // Sử dụng add() đúng cách
+        }
+        return tongTien;
+    }
+
+    public Integer getCount() {
+        return getAll().size();
+    }
+
+}
